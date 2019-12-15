46f080d6618c2f93728a693192f30a11c5c86921
yongjin-shin <yongjin.shinn@gmail.com>
2019-12-16 01:25:34 +0900

    server

--- server_mv.sh
+++ server_mv.sh
@@ -2,3 +2,4 @@ cp transformerdpp.py /home/celab/kimkyu80/myenv/lib/python3.7/site-packages/fair
 cp multihead_attention_DPP.py /home/celab/kimkyu80/myenv/lib/python3.7/site-packages/fairseq/models/multihead_attention_DPP.py
 cp transformerdpp_layer.py /home/celab/kimkyu80/myenv/lib/python3.7/site-packages/fairseq/models/transformerdpp_layer.py
 cp cross_entropy_dpp.py /home/celab/kimkyu80/myenv/lib/python3.7/site-packages/fairseq/criterions/cross_entropy_dpp.py
+cp label_smoothed_cross_entropy_dpp.py /home/celab/kimkyu80/myenv/lib/python3.7/site-packages/fairseq/criterions/label_smoothed_cross_entropy_dpp.py
